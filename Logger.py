6f76faea5e5822d1fb19bd56784bd2610114844b
Alex Irvine <alex.b.irvine@gmail.com>
2021-08-17 22:26:24 -0400

    Data prep complete

--- Logger.py
+++ Logger.py
@@ -6,7 +6,7 @@ logging.basicConfig(level = logging.DEBUG)
 main_logger = logging.getLogger(__name__)
 
 # Create handlers
-file_handler = logging.FileHandler('./app.log', 'w')
+file_handler = logging.FileHandler('./app.log', 'w')  # Setting to 'w' erases log on startup.  
 file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_format)
 file_handler.setLevel(logging.DEBUG)
